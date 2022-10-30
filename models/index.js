b6f1855b2cc25f08d5990ebd37498f2fb71a338b
swimming <sooyoung209@naver.com>
2022-10-30 23:53:28 +0900

    Feat: 회원 팔로우 모델 추가

--- models/index.js
+++ models/index.js
@@ -1,28 +1,40 @@
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
+const { Sequelize, DataTypes } = require('sequelize');
 const env = process.env.NODE_ENV || 'dev';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+const User = require('./user')(sequelize, DataTypes);
+const UserFollowUser = sequelize.define('UserFollowUser', {
+  userId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: User,
+      key: 'id'
+    }
+  },
+  followId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: User,
+      key: 'id'
+    }
+  },
+}, {
+  tableName: 'UserFollowUser'
 });
+// const Article = require('./article')(sequelize, DataType)
+// const Comment = require('./comment')(sequelize, DataType)
+// const Tag = require('./tag')(sequelize, DataType)
+
+// Associations.
+User.belongsToMany(User, { through: UserFollowUser, as: 'follows', foreignKey: 'userId', otherKey: 'followId' });
+UserFollowUser.belongsTo(User, { foreignKey: 'userId' })
+User.hasMany(UserFollowUser, { foreignKey: 'followId' })
+
+db.User = User;
+db.UserFollowUser = UserFollowUser;
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
