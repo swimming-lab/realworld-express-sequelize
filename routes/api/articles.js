9d5ad189cf5ad2c56c6027ea154dc7ef54939c5d
swimming <sooyoung209@naver.com>
2022-11-03 23:28:03 +0900

    Feat: Article Favoriate 기능 추가

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -6,6 +6,31 @@ router.get('/', (req, res, next) => {
   res.send('respond with a resource');
 });
 
+router.param('article', async (req, res, next, slug) => {
+  await Article.findOne({ where: {slug: slug} })
+    .then((article) => {
+      if (!article) { res.sendStatus(404); }
+
+      req.article = article;
+      console.log("조회 성공: ", article);
+
+      next();
+    })
+    .catch(next);
+});
+
+async function setArticleTags(req, article, tagList) {
+	await Tag.bulkCreate(
+		tagList.map((tag) => { return { name: tag } }), 
+		{ ignoreDuplicates: true }
+	);
+
+	// IDs may be missing from the above, so we have to do a find.
+	// https://github.com/sequelize/sequelize/issues/11223#issuecomment-864185973
+	const tags = await Tag.findAll({ where: {name: tagList} });
+	await article.setTags(tags);
+}
+
 router.post('/', auth.required, async (req, res, next) => {
 	try {
     const user = await User.findByPk(req.auth.id);
@@ -26,20 +51,28 @@ router.post('/', auth.required, async (req, res, next) => {
   }
 });
 
-async function setArticleTags(req, article, tagList) {
+router.post('/:article/favorite', auth.required, async (req, res, next) => {
 	try {
-		await Tag.bulkCreate(
-			tagList.map((tag) => { return { name: tag } }), 
-			{ ignoreDuplicates: true }
-		);
+		const user = await User.findByPk(req.auth.id);
+		if (!user) { res.sendStatus(401); }
 
-		// IDs may be missing from the above, so we have to do a find.
-    // https://github.com/sequelize/sequelize/issues/11223#issuecomment-864185973
-		const tags = await Tag.findAll({ where: {name: tagList} });
-		return await article.setTags(tags);
-	} catch(err) {
+		await user.addFavorite(req.article.id);
+    res.json({ article: await req.article.toJSONFor(user) });
+  } catch(err) {
     next(err);
   }
-}
+});
+
+router.delete('/:article/favorite', auth.required, async (req, res, next) => {
+	try {
+		const user = await User.findByPk(req.auth.id);
+		if (!user) { res.sendStatus(401); }
+
+		await user.removeFavorite(req.article.id);
+    res.json({ article: await req.article.toJSONFor(user) });
+  } catch(err) {
+    next(err);
+  }
+});
 
 module.exports = router;
